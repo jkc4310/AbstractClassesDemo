625971e0a69e1058482fec2c248548da6071ffd2
kendall chapman <jkc4310@gmail.com>
2023-08-22 16:30:06 -0400

    abstract class project

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public class Motorcycle : Vehicle
+	{
+		public Motorcycle()
+		{
+		}
+
+        public bool HasSideCar { get; set; } = true;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcycle is in drive.");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType} that is virtually in drive");
+        }
+    }
+}
+
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,32 +17,50 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+             * DONE - Create an abstract class called Vehicle
+             * DONE - The vehicle class shall have three string properties Year, Make, and Model
+             * DONE - Set the defaults to something generic in the Vehicle class
+             * DONE - Vehicle shall have an abstract method called DriveAbstract with no implementation
+             * DONE - Vehicle shall have a virtual method called DriveVirtual with a base implementation.
              */
 
             /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
+             * DONE - Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+             * DONE - Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * DONE - Provide the implementations for the abstract methods
+             * DONE - Only in the Motorcycle class will you override the virtual drive method
             */
 
             // Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * 
              * Set the properties with object initializer syntax
              */
+            var focus = new Car() { HasTrunk = true, Make = "Ford", Model = "Focus", Year = 2013 };
+            var motorcycle = new Motorcycle() { HasSideCar = true, Make = "Harley Davidson", Model = "Sportster 72", Year = 1978 };
+
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Dodge", Model = "something", Year = 2011 };
+            Vehicle van = new Car() { HasTrunk = true, Make = "Honda", Model = "Odyssey ", Year = 2000 };
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+
+            vehicles.Add(focus);
+            vehicles.Add(motorcycle);
+            vehicles.Add(sedan);
+            vehicles.Add(van);
+
+            foreach (var veh in vehicles)
+            {
+                Console.WriteLine($"Make {veh.Make} Model {veh.Model} Year {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine("-----------------------");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
